b7c4243dd876f35e40d768f2268c028dffd9cb6e
Junior Dugue <jrdugue@gmail.com>
2019-06-08 08:29:39 -0400

    destructured the props and added conditional statement
    destructured the props and added conditional statement

--- src/components/Slots.js
+++ src/components/Slots.js
@@ -2,12 +2,24 @@ import React from "react";
 
 class Slots extends React.Component {
   render() {
+    const { s1, s2, s3 } = this.props;
+
+    const winner = s1 === s2 && s2 === s3;
+    // const winner = (this.props.s1 === this.props.s2) && (this.props.s2 === this.props.s3);
     return (
       <div>
+        {/* 
+        with destructurng, we can just simply use the props name
         <p>{this.props.s1} {this.props.s2} {this.props.s3}</p>
-        <p>Winner/Loser</p>
+        before and after
+        <p>{s1} {s2} {s3}</p>
+        */}
+        <p>
+          {s1} {s2} {s3}
+        </p>
+        <p>{winner ? "Winner!" : "Loser!"}</p>
       </div>
-    )
+    );
   }
 }
 
